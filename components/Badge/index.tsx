49cb788d97ec4b009c6c8895e7446be1a1463dce
xzdry <dry@xzdry.net>
2022-01-14 17:21:58 +0800

    feat: color property can be used for non-dot badge

--- components/Badge/index.tsx
+++ components/Badge/index.tsx
@@ -93,7 +93,7 @@ function Badge(baseProps: BadgeProps, ref) {
         </span>
       );
     }
-    if ((dot || color) && count > 0) {
+    if (dot && count > 0) {
       return (
         <CSSTransition
           classNames="badge-zoom"
@@ -119,7 +119,13 @@ function Badge(baseProps: BadgeProps, ref) {
     return (
       <Count
         prefixCls={prefixCls}
-        className={cs(`${prefixCls}-number`, dotClassName)}
+        className={cs(
+          `${prefixCls}-number`,
+          {
+            [`${prefixCls}-color-${color}`]: color,
+          },
+          dotClassName
+        )}
         style={{ ...colorStyle, ...dotStyle }}
         maxCount={maxCount}
         count={count}
