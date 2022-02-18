8d775f393d1e85e00d7198d3f665b3932569c401
pengjiyuan <pengjiyuan@bytedance.com>
2022-02-18 15:38:05 +0800

    feat: DatePicker support timezone

--- stories/components/DatePicker.jsx
+++ stories/components/DatePicker.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
-// import dayjs from 'dayjs';
-import { DatePicker } from '@self';
+import dayjs from 'dayjs';
+// import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import { ConfigProvider, DatePicker } from '@self';
 
-const { MonthPicker } = DatePicker;
+// dayjs.extend(utc);
+dayjs.extend(timezone);
 
-class Demo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+// dayjs.tz.setDefault("America/New_York")
+// dayjs.tz.setDefault("Europe/London")
 
-  render() {
-    return (
-      <div>
-        <DatePicker placeholder="请选择日期" />
-        <hr />
-        <MonthPicker placeholder="请选择月份" />
-      </div>
-    );
-  }
+// const { MonthPicker } = DatePicker;
+
+function Demo() {
+  // console.log(dayjs().toDate().getTimezoneOffset())
+  const date1 = new Date('August 19, 1975 23:15:30 GMT+07:00');
+  // console.log(dayjs.utc().utcOffset(2).format());
+  // console.log(dayjs.utc().utcOffset(0).format());
+  // console.log(dayjs("2014-06-01 12:00").tz("America/New_York").format())
+  // const time = dayjs('2022-01-05').utcOffset(0, true).add(1, 'minute');
+  // console.log(time, time.format())
+  // console.log(dayjs.tz('2022-01-10', 'Europe/London').week())
+  // console.log(dayjs.tz('2022-01-05').add(1, 'minute').format());
+  // console.log(date1)
+  return (
+    <ConfigProvider utcOffset={1}>
+      <DatePicker showTime onChange={(v, dv) => console.log(v, dv)} />
+    </ConfigProvider>
+  );
 }
 
 export default Demo;
