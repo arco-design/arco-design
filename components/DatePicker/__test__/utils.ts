73eee675336f60031d4a508b9f6e2655231eebca
pengjiyuan <pengjiyuan@bytedance.com>
2022-03-03 16:28:19 +0800

    test: DatePicker add test

--- components/DatePicker/__test__/utils.ts
+++ components/DatePicker/__test__/utils.ts
@@ -5,3 +5,33 @@ export function getDateCell(component, panelIndex, cellIndex) {
 export function getInput(component, index) {
   return component.find('input').at(index);
 }
+
+function getSelectedTime(component, index) {
+  return component
+    .find('.arco-timepicker-list')
+    .at(index)
+    .find('.arco-timepicker-cell-selected')
+    .text();
+}
+
+export function checkTime(component, hour, minute, second) {
+  expect(getSelectedTime(component, 0)).toBe(hour);
+  expect(getSelectedTime(component, 1)).toBe(minute);
+  expect(getSelectedTime(component, 2)).toBe(second);
+}
+
+function getSelectedRangeTime(component, dateIndex, index) {
+  return component
+    .find('.arco-panel-date')
+    .at(dateIndex)
+    .find('.arco-timepicker-list')
+    .at(index)
+    .find('.arco-timepicker-cell-selected')
+    .text();
+}
+
+export function checkRangeTime(component, dateIndex, hour, minute, second) {
+  expect(getSelectedRangeTime(component, dateIndex, 0)).toBe(hour);
+  expect(getSelectedRangeTime(component, dateIndex, 1)).toBe(minute);
+  expect(getSelectedRangeTime(component, dateIndex, 2)).toBe(second);
+}
