e988e86332c27172486c04522c2b8a8cebe6c0ab
zuozhiheng <zuozhiheng@bytedance.com>
2022-03-23 17:50:17 +0800

    chore: update docgen config to avoid generating property document from HTML element

--- .config/docgen.config.js
+++ .config/docgen.config.js
@@ -2,5 +2,5 @@ module.exports = (config) => {
   config.languages = ['zh-CN', 'en-US'];
   config.template = '__template__/index.[language].md';
   config.outputFileName = 'README.[language].md';
-  config.tsParseTool = ['ts-document'];
+  config.tsParseTool = ['ts-document', { strictComment: true }];
 };
