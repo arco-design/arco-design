5734eb2ba63b3890a6ba2f5cfc888bc1dda07a07
chenc <double_cl@163.com>
2022-03-31 22:23:35 +0800

    chore: update test case

--- components/Modal/__test__/index.test.tsx
+++ components/Modal/__test__/index.test.tsx
@@ -120,7 +120,33 @@ describe('Modal', () => {
     expect($('.arco-modal-wrapper')[0].style.display).toBe('none');
   });
 
-  it('close Modal with esc keydown event', () => {
+  it('close Modal with esc keydown event and focusLock is false', () => {
+    jest.useFakeTimers();
+    const onCancel = jest.fn();
+    Modal.confirm({
+      title: 'title',
+      content: 'content',
+      focusLock: false,
+      onCancel,
+    });
+    Modal.error({
+      title: 'title',
+      content: 'content',
+      focusLock: false,
+      onCancel,
+    });
+    jest.runAllTimers();
+    expect(document.querySelectorAll(`.arco-modal-wrapper`)).toHaveLength(2);
+    TestUtils.Simulate.keyDown(document.querySelectorAll('.arco-modal-wrapper')[0], {
+      key: Esc.key,
+    });
+    jest.runAllTimers();
+    expect(document.querySelectorAll(`.arco-modal-wrapper`)).toHaveLength(1);
+    expect(onCancel).toHaveBeenCalledTimes(1);
+    jest.useRealTimers();
+  });
+
+  it('close Modal with esc keydown event and focusLock and autoFocus is true', () => {
     jest.useFakeTimers();
     const onCancel = jest.fn();
     Modal.confirm({
@@ -135,7 +161,7 @@ describe('Modal', () => {
     });
     jest.runAllTimers();
     expect(document.querySelectorAll(`.arco-modal-wrapper`)).toHaveLength(2);
-    TestUtils.Simulate.keyDown(document.querySelectorAll('.arco-modal-wrapper')[0], {
+    TestUtils.Simulate.keyDown(document.querySelectorAll('[data-focus-lock-disabled]')[0], {
       key: Esc.key,
     });
     jest.runAllTimers();
