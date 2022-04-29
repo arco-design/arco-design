fea082a6dde13e5d0c7a11939d8a9cb0ebe3e4ab
yuhan0709 <853654529@qq.com>
2022-04-29 18:55:54 +0800

    fix(Pagination): Table Pagination 优先级 (#827)

--- components/Table/__test__/pagination.test.tsx
+++ components/Table/__test__/pagination.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Table from '..';
+import { ConfigProvider } from '../..';
 import { columnsFilterCustom } from './common/columns';
 import { data } from './common/data';
 
@@ -40,5 +41,36 @@ describe('Table test', () => {
   it('table has no data, the pagination should disappear', () => {
     const component = mount(<Table data={[]} columns={columnsFilterCustom} />);
     expect(component.find('.arco-pagination').exists()).toBeFalsy();
+  });
+
+  it('table pagination has correctly priority when set configProvider', () => {
+    const data = new Array(20)
+      .fill('0')
+      .map((_, index) => ({ key: `key${index}`, value: `value${index}` }));
+    const columns = ['name', 'value'].map((key) => ({ title: key, dataIndex: key }));
+    const component = mount(
+      <ConfigProvider
+        tablePagination={{ pageSize: 5 }}
+        componentConfig={{ Table: { pagination: { pageSize: 10 } } }}
+      >
+        {/* props.pagination =>  4 */}
+        <Table data={data} columns={columns} pagination={{ pageSize: 4 }} className="table-1" />
+
+        <ConfigProvider componentConfig={{ Table: { pagination: { pageSize: 6 } } }}>
+          {/* componentConfig.Table.Pagination => 6 */}
+          <Table data={data} columns={columns} className="table-2" />
+          {/* no Pagination => total => 20 */}
+          <Table data={data} columns={columns} pagination={false} className="table-3" />
+        </ConfigProvider>
+
+        {/* tablePagination => 5 */}
+        <Table data={data} columns={columns} className="table-4" />
+      </ConfigProvider>
+    );
+
+    expect(component.find('.table-1').find('tbody').find('tr')).toHaveLength(4);
+    expect(component.find('.table-2').find('tbody').find('tr')).toHaveLength(6);
+    expect(component.find('.table-3').find('tbody').find('tr')).toHaveLength(20);
+    expect(component.find('.table-4').find('tbody').find('tr')).toHaveLength(5);
   });
 });
