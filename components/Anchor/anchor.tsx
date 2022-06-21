568c7ccab83c7085a648b015d5f753b31ef8bbb8
yinkaihui <yinkaihui@bytedance.com>
2022-06-21 16:06:16 +0800

    test: update test cases

--- components/Anchor/anchor.tsx
+++ components/Anchor/anchor.tsx
@@ -97,6 +97,9 @@ function Anchor(baseProps: AnchorPropsWithChildren, ref) {
 
   const setActiveLink = useCallback(
     (hash: string) => {
+      if (hash === '#a') {
+        console.log(hash, !wrapperRef.current, linkMap.current, '___________');
+      }
       if (!hash || !wrapperRef.current) return;
       // Try to add when there is no corresponding link
       if (!linkMap.current.has(hash)) {
