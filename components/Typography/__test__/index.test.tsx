f0ca48fcecf2f65ddca4f8115e4b11b84776b087
yangyuhan.79 <yangyuhan.79@bytedance.com>
2022-07-01 17:01:54 +0800

    refactor: Typography test case

--- components/Typography/__test__/index.test.tsx
+++ components/Typography/__test__/index.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { mount } from 'enzyme';
 import { act } from 'react-test-renderer';
 import Typography from '..';
 import IconCopy from '../../../icon/react-icon/IconCopy';
-import { sleep } from '../../../tests/util';
+import { sleep, render, fireEvent } from '../../../tests/util';
 import copy from '../../_util/clipboard';
 
 const { Title, Text, Paragraph } = Typography;
@@ -20,7 +19,7 @@ jest.mock('../../_util/clipboard', () => {
 
 describe('Typography', () => {
   it('basic bold', () => {
-    const wrapper = mount(<Text bold mark code underline delete />);
+    const wrapper = render(<Text bold mark code underline delete />);
     expect(wrapper.find('.arco-typography')).toHaveLength(1);
     expect(wrapper.find('b')).toHaveLength(1);
     expect(wrapper.find('mark')).toHaveLength(1);
@@ -30,36 +29,36 @@ describe('Typography', () => {
   });
 
   it('basic title Render', () => {
-    const wrapper = mount(<Title heading={1}>Basic Title</Title>);
+    const wrapper = render(<Title heading={1}>Basic Title</Title>);
     expect(wrapper.find('h1')).toHaveLength(1);
 
     act(() => {
-      wrapper.setProps({ heading: 3 });
-      wrapper.update();
+      wrapper.rerender(<Title heading={3}>Basic Title</Title>);
     });
 
     expect(wrapper.find('h3')).toHaveLength(1);
   });
 
   it('specal Paragraph render', () => {
-    const wrapper = mount(
+    const wrapper = render(
       <Paragraph className="paragraph-test" spacing="close">
         Paragraph with customize className
       </Paragraph>
     );
 
-    expect(wrapper.find('Base').hasClass('paragraph-test')).toBeTruthy();
-    expect(wrapper.find('Base').hasClass('arco-typography-spacing-close')).toBeTruthy();
+    expect(wrapper.find('.arco-typography')[0].classList.contains('paragraph-test')).toBeTruthy();
+    expect(
+      wrapper.find('.arco-typography')[0].classList.contains('arco-typography-spacing-close')
+    ).toBeTruthy();
   });
 
   it('support copyable', async () => {
     const onCopy = jest.fn();
-    const wrapper = mount(<Text copyable={{ onCopy, icon: <IconCopy /> }}>copyable test</Text>);
-    expect(wrapper.find('IconCopy')).toHaveLength(1);
+    const wrapper = render(<Text copyable={{ onCopy, icon: <IconCopy /> }}>copyable test</Text>);
+    expect(wrapper.find('.arco-icon-copy')).toHaveLength(1);
 
     act(() => {
-      wrapper.find('.arco-typography-operation-copy').simulate('click');
-      wrapper.update();
+      fireEvent.click(wrapper.find('.arco-typography-operation-copy')[0]);
     });
 
     expect(copy).toHaveBeenCalled();
@@ -69,8 +68,8 @@ describe('Typography', () => {
       sleep(3000);
     });
 
-    expect(wrapper.find('IconCopy')).toHaveLength(0);
-    expect(wrapper.find('IconCheckCircleFill')).toHaveLength(1);
+    expect(wrapper.find('.arco-icon-copy')).toHaveLength(0);
+    expect(wrapper.find('.arco-icon-check-circle-fill')).toHaveLength(1);
   });
 
   it('support editable correctly', () => {
@@ -80,34 +79,25 @@ describe('Typography', () => {
     const onStart = jest.fn();
     const onEnd = jest.fn();
     const onChange = jest.fn();
-    const wrapper = mount(
+    const wrapper = render(
       <Paragraph editable={{ onChange, onStart, onEnd }}>{beforeText}</Paragraph>
     );
     act(() => {
-      wrapper.find('.arco-typography-operation-edit').simulate('click');
-      wrapper.update();
+      fireEvent.click(wrapper.find('.arco-typography-operation-edit')[0]);
     });
 
     expect(onStart).toHaveBeenCalledTimes(1);
-    expect(wrapper.find('TextArea')).toHaveLength(1);
+    expect(wrapper.find('.arco-textarea')).toHaveLength(1);
 
     act(() => {
-      wrapper.find('TextArea').simulate('change', {
-        target: {
-          value: afterText,
-        },
-      });
-      wrapper.update();
+      fireEvent.change(wrapper.find('.arco-textarea')[0], { target: { value: afterText } });
     });
-
     expect(onChange.mock.calls[0][0]).toEqual(afterText);
     act(() => {
-      wrapper.find('TextArea').simulate('blur');
-      wrapper.setProps({
-        children: afterText,
-      });
+      fireEvent.blur(wrapper.find('.arco-textarea')[0]);
+      wrapper.rerender(<Paragraph editable={{ onChange, onStart, onEnd }}>{afterText}</Paragraph>);
     });
     expect(onEnd).toHaveBeenCalledTimes(1);
-    expect(wrapper.text()).toEqual(afterText);
+    expect(wrapper.find('.arco-typography')[0]).toHaveTextContent(afterText);
   });
 });
