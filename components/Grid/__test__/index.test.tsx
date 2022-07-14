0ac41cea4499c82c3b4fc92d6a9841e2f9d823be
zuozhiheng <zuozhiheng@bytedance.com>
2022-07-14 16:17:33 +0800

    test: update test cases of Grid

--- components/Grid/__test__/index.test.tsx
+++ components/Grid/__test__/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { render } from '../../../tests/util';
 import mountTest from '../../../tests/mountTest';
 import componentConfigTest from '../../../tests/componentConfigTest';
 import Grid from '..';
@@ -12,59 +12,67 @@ mountTest(Col);
 componentConfigTest(Grid.Row, 'Grid.Row');
 componentConfigTest(Grid.Col, 'Grid.Col');
 
-it('render text and gutter correctly', () => {
-  const grid = mount(
-    <Row gutter={12}>
-      <Col span={12}>
-        <div>Col-span-12 gutter-12</div>
-      </Col>
-      <Col span={12}>
-        <div>Col-span-12 gutter-12</div>
-      </Col>
-    </Row>
-  );
-
-  grid.find('.arco-col').forEach((node) => {
-    expect(node.prop('style')).toEqual({ paddingLeft: 6, paddingRight: 6 });
-  });
-});
-
-it('gutter correct when row children is custom component', () => {
-  function CustomCol(props) {
-    const { children, ...rest } = props;
-    return (
-      <Col {...rest}>
-        <div>{children}</div>
-      </Col>
+describe('Grid', () => {
+  it('render text and gutter correctly', () => {
+    render(
+      <Row gutter={12}>
+        <Col span={12}>
+          <div>Col-span-12 gutter-12</div>
+        </Col>
+        <Col span={12}>
+          <div>Col-span-12 gutter-12</div>
+        </Col>
+      </Row>
     );
-  }
 
-  const grid = mount(
-    <Row gutter={12}>
-      <CustomCol span={12}>
-        <div>Col-span-12 gutter-12</div>
-      </CustomCol>
-      <CustomCol span={12}>
-        <div>Col-span-12 gutter-12</div>
-      </CustomCol>
-    </Row>
-  );
-
-  grid.find('.arco-col').forEach((node) => {
-    expect(node.prop('style')).toEqual({ paddingLeft: 6, paddingRight: 6 });
+    document.querySelectorAll('.arco-col').forEach((node) => {
+      const style = getComputedStyle(node);
+      expect(style.paddingLeft).toBe('6px');
+      expect(style.paddingRight).toBe('6px');
+    });
   });
-});
 
-it('flex', () => {
-  const grid = mount(
-    <Row>
-      <Col flex="100px">100px</Col>
-      <Col flex="auto">auto</Col>
-      <Col flex="1 1 auto">1 1 auto</Col>
-    </Row>
-  );
+  it('gutter correct when row children is custom component', () => {
+    function CustomCol(props) {
+      const { children, ...rest } = props;
+      return (
+        <Col {...rest}>
+          <div>{children}</div>
+        </Col>
+      );
+    }
 
-  expect(grid.find('.arco-col').at(0).prop('style')).toEqual({ flex: '0 0 100px' });
-  expect(grid.find('.arco-col').at(1).prop('style')).toEqual({ flex: 'auto' });
-  expect(grid.find('.arco-col').at(2).prop('style')).toEqual({ flex: '1 1 auto' });
+    render(
+      <Row gutter={12}>
+        <CustomCol span={12}>
+          <div>Col-span-12 gutter-12</div>
+        </CustomCol>
+        <CustomCol span={12}>
+          <div>Col-span-12 gutter-12</div>
+        </CustomCol>
+      </Row>
+    );
+
+    document.querySelectorAll('.arco-col').forEach((node) => {
+      const style = getComputedStyle(node);
+      expect(style.paddingLeft).toBe('6px');
+      expect(style.paddingRight).toBe('6px');
+    });
+  });
+
+  it('flex', () => {
+    render(
+      <Row>
+        <Col flex="100px">100px</Col>
+        <Col flex="auto">auto</Col>
+        <Col flex="1 1 auto">1 1 auto</Col>
+      </Row>
+    );
+
+    const cols = document.querySelectorAll('.arco-col');
+
+    expect(getComputedStyle(cols[0]).flex).toBe('0 0 100px');
+    expect(getComputedStyle(cols[1]).flex).toBe('auto');
+    expect(getComputedStyle(cols[2]).flex).toBe('1 1 auto');
+  });
 });
