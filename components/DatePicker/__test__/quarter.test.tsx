e402831cb0c6ce4c0e24a71de642970156adfbf3
SleepDevil <55879038+SleepDevil@users.noreply.github.com>
2022-07-14 15:44:32 +0800

    test: upgrade test cases (#1134)

--- components/DatePicker/__test__/quarter.test.tsx
+++ components/DatePicker/__test__/quarter.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { render, fireEvent } from '../../../tests/util';
 import mountTest from '../../../tests/mountTest';
 import DatePicker from '..';
 import '../../../tests/mockDate';
@@ -10,33 +10,30 @@ mountTest(QuarterPicker);
 
 describe('QuarterPicker', () => {
   it('defaultValue & today', async () => {
-    const component = mount(<QuarterPicker />);
+    const component = render(<QuarterPicker />);
 
-    component.simulate('click');
+    fireEvent.click(component.container.firstChild!);
 
-    expect(component.find('.arco-picker-cell-today').text()).toBe('Q2');
+    expect(component.find('.arco-picker-cell-today')[0].textContent).toBe('Q2');
 
-    expect(component.find('input').prop('value')).toBe('');
+    expect(component.find('.arco-picker-input input')[0].getAttribute('value')).toBe('');
 
-    component
-      .find('.arco-picker-date')
-      .at(0) // Q1
-      .simulate('click');
+    fireEvent.click(component.find('.arco-picker-date').item(0)); // Q1
 
-    expect(component.find('input').prop('value')).toBe('2020-Q1');
+    expect(component.find('.arco-picker-input input')[0].getAttribute('value')).toBe('2020-Q1');
   });
 
   it('allowClear', () => {
     const onClear = jest.fn();
-    const component = mount(<QuarterPicker defaultValue="2020-Q1" allowClear onClear={onClear} />);
+    const component = render(<QuarterPicker defaultValue="2020-Q1" allowClear onClear={onClear} />);
 
-    component.simulate('click');
+    fireEvent.click(component.container.firstChild!);
 
-    expect(component.find('input').prop('value')).toBe('2020-Q1');
+    expect(component.find('.arco-picker-input input')[0].getAttribute('value')).toBe('2020-Q1');
 
-    component.find('IconClose').simulate('click');
+    fireEvent.click(component.find('.arco-icon-close')[0]);
 
-    expect(component.find('input').prop('value')).toBe('');
+    expect(component.find('.arco-picker-input input')[0].getAttribute('value')).toBe('');
     expect(onClear.mock.calls.length).toBe(1);
   });
 });
