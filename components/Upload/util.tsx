b1039627b942d1c279967e2d605a7087bf2f9c31
SleepDevil <1752915704@qq.com>
2022-08-03 14:42:53 +0800

    fix: fix accept file util

--- components/Upload/util.tsx
+++ components/Upload/util.tsx
@@ -1,6 +1,6 @@
 import { isArray } from '../_util/is';
 
-export const isAcceptFile = (file, accept?: string | string[]): boolean => {
+export const isAcceptFile = (file: File, accept?: string | string[]): boolean => {
   if (accept && file) {
     const accepts = isArray(accept)
       ? accept
@@ -8,24 +8,33 @@ export const isAcceptFile = (file, accept?: string | string[]): boolean => {
           .split(',')
           .map((x) => x.trim())
           .filter((x) => x);
-    const fileExtension = file.name.indexOf('.') > -1 ? file.name.split('.').pop() : '';
+    const fileExtension = file.name.indexOf('.') > -1 ? `.${file.name.split('.').pop()}` : '';
     return accepts.some((type) => {
-      const text = type && type.toLowerCase();
+      const typeText = type && type.toLowerCase();
       const fileType = (file.type || '').toLowerCase();
-      if (text === fileType) {
+      if (typeText === fileType) {
         // 类似excel文件这种
         // 比如application/vnd.ms-excel和application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
         // 本身就带有.字符的，不能走下面的.jpg等文件扩展名判断处理
         // 所以优先对比input的accept类型和文件对象的type值
         return true;
       }
-      if (/\/\*/.test(text)) {
-        // image/* 这种通配的形式处理
-        return fileType.replace(/\/.*$/, '') === text.replace(/\/.*$/, '');
+      // */*,*  之类的所有类型
+      if (/^\*(\/\*)?$/.test(typeText)) {
+        return true;
       }
-      if (/\..*/.test(text)) {
+      if (/\/\*/.test(typeText)) {
+        // image/* 这种通配的形式处理
+        return fileType.replace(/\/.*$/, '') === typeText.replace(/\/.*$/, '');
+      }
+      if (/\..*/.test(typeText)) {
         // .jpg 等后缀名
-        return text === `.${fileExtension && fileExtension.toLowerCase()}`;
+        let suffixList = [typeText];
+        // accept=".jpg", jpeg后缀类型同样可以上传，反之亦然
+        if (typeText === '.jpg' || typeText === '.jpeg') {
+          suffixList = ['.jpg', '.jpeg'];
+        }
+        return suffixList.indexOf(fileExtension) > -1;
       }
       return false;
     });
