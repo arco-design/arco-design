ea53d4e2721b38a4af217a6fa15e954bc6eb45dd
2239559319 <w2239559319@outlook.com>
2022-10-20 19:56:27 +0800

    fix: silder hooks

--- components/Slider/hooks/useLegalValue.ts
+++ components/Slider/hooks/useLegalValue.ts
@@ -1,4 +1,5 @@
 import { plus, times } from 'number-precision';
+import { useCallback } from 'react';
 import { isArray, isNumber, isUndefined, isObject, isEmptyObject } from '../../_util/is';
 import { getPrecision } from '../utils';
 import { IntervalConfig } from './useInterval';
@@ -13,79 +14,97 @@ export default function useLegalValue(props: {
   marks: object;
 }) {
   const { isRange, min, max, onlyMarkValue, intervalConfigs, marks } = props;
-  function getPrecisionValue(val: number): number {
-    const { begin, step } = intervalConfigs.find((config) => {
-      return val >= config.begin && val <= config.end;
-    });
+  const getPrecisionValue = useCallback(
+    (val: number) => {
+      const { begin, step } = intervalConfigs.find((config) => {
+        return val >= config.begin && val <= config.end;
+      });
 
-    const offsetVal = val - begin;
-    const stepNum = Math.round(offsetVal / step);
-    const precision = getPrecision(step);
-    const currentIntervalPrecision = parseFloat(times(step, stepNum).toFixed(precision));
-    return plus(begin, currentIntervalPrecision);
-  }
+      const offsetVal = val - begin;
+      const stepNum = Math.round(offsetVal / step);
+      const precision = getPrecision(step);
+      const currentIntervalPrecision = parseFloat(times(step, stepNum).toFixed(precision));
+      return plus(begin, currentIntervalPrecision);
+    },
+    [intervalConfigs]
+  );
 
   // 在只允许选择 marks 中的值的时候，找到离value最接近的值
-  function getMarkValue(val: number): number {
-    if (!isObject(marks) || isEmptyObject(marks)) {
-      console.warn('marks must be an object when onlyMarkValue is true');
-      return min;
-    }
-    if (marks[val]) {
-      return val;
-    }
-    const keys = Object.keys(marks);
-    const diffs = keys.map((x) => Math.abs(val - parseFloat(x)));
-    const minIndex = diffs.indexOf(Math.min.apply(null, diffs));
-    return parseFloat(keys[minIndex]);
-  }
+  const getMarkValue = useCallback(
+    (val: number) => {
+      if (!isObject(marks) || isEmptyObject(marks)) {
+        console.warn('marks must be an object when onlyMarkValue is true');
+        return min;
+      }
+      if (marks[val]) {
+        return val;
+      }
+      const keys = Object.keys(marks);
+      const diffs = keys.map((x) => Math.abs(val - parseFloat(x)));
+      const minIndex = diffs.indexOf(Math.min.apply(null, diffs));
+      return parseFloat(keys[minIndex]);
+    },
+    [marks, min]
+  );
 
   // 判断值是否在[min, max]区间内，并且满足步长或是标签值
-  function getLegalValue(val: number): number {
-    if (isUndefined(val)) return min;
-    if (val <= min) return min;
-    if (val >= max) return max;
-    if (onlyMarkValue) return getMarkValue(val);
-    return getPrecisionValue(val);
-  }
+  const getLegalValue = useCallback(
+    (val) => {
+      if (isUndefined(val)) return min;
+      if (val <= min) return min;
+      if (val >= max) return max;
+      if (onlyMarkValue) return getMarkValue(val);
+      return getPrecisionValue(val);
+    },
+    [getMarkValue, getPrecisionValue, max, min, onlyMarkValue]
+  );
 
-  function isLegalValue(val) {
-    return getLegalValue(val) === val;
-  }
+  const isLegalValue = useCallback(
+    (val) => {
+      return getLegalValue(val) === val;
+    },
+    [getLegalValue]
+  );
 
   // 获取合法的 range value
-  function getLegalRangeValue(val: number | number[]) {
-    let [beginVal, endVal] = [min, min];
-    if (isRange) {
-      if (isArray(val)) {
-        beginVal = getLegalValue(val[0]);
-        endVal = getLegalValue(val[1]);
+  const getLegalRangeValue = useCallback(
+    (val: number | number[]) => {
+      let [beginVal, endVal] = [min, min];
+      if (isRange) {
+        if (isArray(val)) {
+          beginVal = getLegalValue(val[0]);
+          endVal = getLegalValue(val[1]);
+        } else {
+          console.error('value must be an array when range is true');
+        }
+      } else if (isNumber(val)) {
+        endVal = getLegalValue(val);
       } else {
-        console.error('value must be an array when range is true');
+        console.error('value must be a number when range is false');
       }
-    } else if (isNumber(val)) {
-      endVal = getLegalValue(val);
-    } else {
-      console.error('value must be a number when range is false');
-    }
-    return [beginVal, endVal];
-  }
+      return [beginVal, endVal];
+    },
+    [getLegalValue, isRange, min]
+  );
 
-  function getNextMarkValue(value: number, type: 'addition' | 'subtraction') {
-    // arrow Left or arrowRight
-    const multi = type === 'subtraction' ? -1 : 1;
+  const getNextMarkValue = useCallback(
+    (value: number, type: 'addition' | 'subtraction') => {
+      // arrow Left or arrowRight
+      const multi = type === 'subtraction' ? -1 : 1;
 
-    let newValue = plus(value, multi * props.step);
-    if (props.onlyMarkValue) {
-      const markKeys = Object.keys(props.marks);
-      const currentIndex = markKeys.findIndex((key) => Number(key) === value);
-      newValue =
-        markKeys[currentIndex + multi] !== undefined
-          ? Number(markKeys[currentIndex + multi])
-          : value;
-    }
-    return newValue;
-  }
+      let newValue = plus(value, multi * props.step);
+      if (props.onlyMarkValue) {
+        const markKeys = Object.keys(props.marks);
+        const currentIndex = markKeys.findIndex((key) => Number(key) === value);
+        newValue =
+          markKeys[currentIndex + multi] !== undefined
+            ? Number(markKeys[currentIndex + multi])
+            : value;
+      }
+      return newValue;
+    },
+    [props.marks, props.onlyMarkValue, props.step]
+  );
 
   return {
     getLegalRangeValue,
