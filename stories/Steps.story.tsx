27c330a6680d474faf9f620e2e6fe5126718e515
yangyuhan.79 <yangyuhan.79@bytedance.com>
2022-11-11 17:49:34 +0800

    style(Steps): steps are used as children of steps

--- stories/Steps.story.tsx
+++ stories/Steps.story.tsx
@@ -1,74 +1,42 @@
 /* eslint-disable no-console */
-import React, { useState } from 'react';
-import { Steps, Radio, StepsProps } from '@self';
+import React from 'react';
+import { Space, Steps } from '@self';
 
 const Step = Steps.Step;
 
 function DemoSteps() {
-  const [current, setCurrent] = useState(2);
-  const [size, setSize] = useState<StepsProps['size']>('default');
-  const [type, setType] = useState<StepsProps['type']>('default');
-  const [direction, setDirection] = useState<StepsProps['direction']>('horizontal');
-
   return (
-    <div>
-      <div style={{ margin: '20px 0' }}>
-        <Radio.Group
-          type="button"
-          value={direction}
-          onChange={(direction) => {
-            setDirection(direction);
-          }}
-        >
-          <Radio value="vertical">vertical</Radio>
-          <Radio value="horizontal">horizontal</Radio>
-        </Radio.Group>
-      </div>
-      <div style={{ margin: '20px 0' }}>
-        <Radio.Group
-          type="button"
-          value={size}
-          onChange={(size) => {
-            setSize(size);
-          }}
-        >
-          <Radio value="small">small</Radio>
-          <Radio value="default">default</Radio>
-        </Radio.Group>
-      </div>
-      <div style={{ margin: '20px 0' }}>
-        <Radio.Group
-          type="button"
-          value={type}
-          onChange={(type) => {
-            setType(type);
-          }}
-        >
-          <Radio value="default">default</Radio>
-          <Radio value="arrow">arrow</Radio>
-          <Radio value="dot">dot</Radio>
-          <Radio value="navigation">navigation</Radio>
-        </Radio.Group>
-      </div>
-      <Steps
-        size={size}
-        type={type}
-        direction={direction}
-        current={current}
-        onChange={(current, id) => {
-          setCurrent(current);
-          console.log(`${current} -- ${id}`);
-        }}
-      >
+    <Space direction="vertical" size={20}>
+      <Steps direction="vertical" current={2}>
+        <Step title="Succeeded" description="This is a description" />
         <Step
-          id="one"
-          title="Step1"
-          description="This is description,This is description,This is description,This is description."
+          title="Processing"
+          description={
+            <Steps current={2} size="small">
+              <Step title="Succeeded" description="This is a description" />
+              <Step title="Processing" description="This is a description" />
+              <Step title="Pending" description="This is a description" />
+            </Steps>
+          }
         />
-        <Step id="two" title="Step2" description="This is description~" />
-        <Step id="three" title="Step3" description="This is description~" />
+        <Step title="Pending" description="This is a description" />
       </Steps>
-    </div>
+
+      <Steps direction="vertical" current={2} type="dot">
+        <Step title="Succeeded" description="This is a description" />
+        <Step
+          title="Processing"
+          description={
+            <Steps current={2} size="small" type="dot">
+              <Step title="Succeeded" description="This is a description" />
+              <Step title="Processing" description="This is a description" />
+              <Step title="Pending" description="This is a description" />
+            </Steps>
+          }
+        />
+        <Step title="Pending" description="This is a description" />
+      </Steps>
+    </Space>
   );
 }
 
