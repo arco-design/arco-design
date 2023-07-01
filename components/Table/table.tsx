b634583066793d4c21ca9362c611306d391b612b
hkhere <hk@tinyclouds.cn>
2023-07-01 22:35:29 +0800

    fix(Table): use dom.getBoundingClientRect().width to get the actual width of the table

--- components/Table/table.tsx
+++ components/Table/table.tsx
@@ -482,7 +482,7 @@ function Table<T extends unknown>(baseProps: TableProps<T>, ref: React.Ref<Table
       const ele =
         root.querySelector(`.${prefixCls}-body`) ||
         root.querySelector(`.${prefixCls}-content-inner`);
-      const tableViewWidth = ele.clientWidth;
+      const tableViewWidth = ele.getBoundingClientRect().width;
       setTableViewWidth(tableViewWidth);
     }
   }
