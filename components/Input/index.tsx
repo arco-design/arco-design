c89842b0f29f55830124fb0d05e68246e4dcd8eb
WindrunnerMax <651525974@qq.com>
2023-07-28 11:16:27 +0800

    chore: export InputType

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,9 +1,17 @@
 import Input from './input';
 
-import { InputProps, TextAreaProps } from './interface';
+import { InputProps, TextAreaProps, RefInputType } from './interface';
 import { InputPasswordProps } from './password';
 import { InputSearchProps } from './search';
+import { RefTextAreaType } from './textarea';
 
-export { InputProps, TextAreaProps, InputPasswordProps, InputSearchProps };
+export {
+  InputProps,
+  TextAreaProps,
+  InputPasswordProps,
+  InputSearchProps,
+  RefInputType,
+  RefTextAreaType,
+};
 
 export default Input;
