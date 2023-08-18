b82de8a4aebd4eb71b20bc0a49394e86289ce227
yinkaihui <yinkaihui@bytedance.com>
2023-08-18 20:16:45 +0800

    style: tabs style in rtl & veritcal

--- stories/Tabs.story.tsx
+++ stories/Tabs.story.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
-import { Tabs, Typography, Radio } from '@self';
+import { Tabs, Typography, Radio, Input } from '@self';
 
 const TabPane = Tabs.TabPane;
-let count = 5;
+const count = 5;
 
 const style: any = {
   textAlign: 'center',
   marginTop: 20,
+  border: '1px solid',
 };
 
 function DemoTabs() {
   const [type, setType] = useState('line');
-  const [position, setPosition] = useState('top');
+  const [position, setPosition] = useState('left');
   return (
     <div>
       <span style={{ marginRight: 20 }}>Size:</span>
@@ -22,7 +23,7 @@ function DemoTabs() {
         onChange={setPosition}
         style={{ marginBottom: 40 }}
         options={['left', 'top', 'bottom', 'right']}
-      ></Radio.Group>
+      />
       <br />
       <span style={{ marginRight: 20 }}>Type:</span>
       <Radio.Group name="type" value={type} onChange={setType} style={{ marginBottom: 40 }}>
@@ -36,16 +37,32 @@ function DemoTabs() {
 
       <Tabs tabPosition={position as any} type={type as any}>
         <TabPane key="1" title="Tab 1">
-          <Typography.Paragraph style={style}>Content of Tab Panel 1</Typography.Paragraph>
+          <Typography.Paragraph style={style}>
+            <div style={{ height: 100 }} />
+            Content of Tab Panel 1
+            <Input />
+          </Typography.Paragraph>
         </TabPane>
         <TabPane key="2" title="Tab 2" disabled>
-          <Typography.Paragraph style={style}>Content of Tab Panel 2</Typography.Paragraph>
+          <Typography.Paragraph style={style}>
+            <div style={{ height: 100 }} />
+            Content of Tab Panel 2
+            <Input />
+          </Typography.Paragraph>
         </TabPane>
         <TabPane key="3" title="Tab 3">
-          <Typography.Paragraph style={style}>Content of Tab Panel 3</Typography.Paragraph>
+          <Typography.Paragraph style={style}>
+            <div style={{ height: 100 }} />
+            Content of Tab Panel 3
+            <Input />
+          </Typography.Paragraph>
         </TabPane>
         <TabPane key="4" title="Tab 4">
-          <Typography.Paragraph style={style}>Content of Tab Panel 4</Typography.Paragraph>
+          <Typography.Paragraph style={style}>
+            <div style={{ height: 100 }} />
+            Content of Tab Panel 4
+            <Input />
+          </Typography.Paragraph>
         </TabPane>
       </Tabs>
     </div>
