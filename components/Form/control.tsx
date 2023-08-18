4d51427649270a913eace3208dfa436a62cffd4d
xiaoyao <15558110117@163.com>
2023-08-18 14:13:06 +0800

    fix(Form): 移除废弃的context用法

--- components/Form/control.tsx
+++ components/Form/control.tsx
@@ -58,13 +58,10 @@ export default class Control<
 
   private removeRegisterField: () => void;
 
-  constructor(
-    props: FormControlProps<FormData, FieldValue, FieldKey>,
-    context: FormItemContextProps<FormData, FieldValue, FieldKey>
-  ) {
+  constructor(props: FormControlProps<FormData, FieldValue, FieldKey>) {
     super(props);
     if ('initialValue' in props && this.hasFieldProps()) {
-      const innerMethods = context.store.getInnerMethods(true);
+      const innerMethods = this.context.store.getInnerMethods(true);
       innerMethods.innerSetInitialValue(props.field, props.initialValue);
     }
   }
