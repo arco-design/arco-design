8d5af9f827f435cf89a26715e01a206054ffd61e
zuozhiheng <zuozhiheng@bytedance.com>
2023-09-08 16:58:08 +0800

    fix(Image): image shows error placeholder during first render

--- components/_util/hooks/useInView.ts
+++ components/_util/hooks/useInView.ts
@@ -3,15 +3,15 @@ import useIntersectionObserver, { supportIntersectionObserver } from './useInter
 
 interface UserInViewProps extends IntersectionObserverInit {
   onChange?: (inView: boolean, entry: IntersectionObserverEntry) => void;
-  hasInView?: boolean;
+  defaultInView?: boolean;
   unobserverOnEnter?: boolean;
   target?: Element;
 }
 
 function useInView(props: UserInViewProps) {
-  const { hasInView, unobserverOnEnter = true, onChange, target, ...rest } = props;
+  const { defaultInView, unobserverOnEnter = true, onChange, target, ...rest } = props;
   // if intersect observer is not supported
-  const [inView, setInView] = useState(supportIntersectionObserver ? hasInView : true);
+  const [inView, setInView] = useState(supportIntersectionObserver ? defaultInView : true);
   const observerCallback = useCallback<IntersectionObserverCallback>(
     ([entry], observer) => {
       const inThreshold = observer.thresholds.some((t) => entry.intersectionRatio >= t);
@@ -28,14 +28,14 @@ function useInView(props: UserInViewProps) {
   const { cor, dor, observer } = useIntersectionObserver(observerCallback, rest);
 
   useEffect(() => {
-    const noNeedObserver = hasInView && unobserverOnEnter;
+    const noNeedObserver = defaultInView && unobserverOnEnter;
     if (noNeedObserver) {
       dor();
     } else if (target) {
       cor(target);
     }
     return dor;
-  }, [target, hasInView, unobserverOnEnter]);
+  }, [target, defaultInView, unobserverOnEnter]);
 
   return {
     inView,
