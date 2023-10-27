4f928680086b48b656f3882e7fa873218d350c96
Flsion <wangchen.flsion@bytedance.com>
2023-10-27 04:28:52 -0500

    enhance(Image): preview-group hot-key support (#2320)

--- stories/Image.story.tsx
+++ stories/Image.story.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Skeleton, Space } from '@self';
+import { Image, Button, Space } from '@self';
 
 const wrapperStyle = {
   padding: 20,
@@ -12,27 +12,36 @@ const wrapperStyle = {
 
 function Demo1() {
   const srcList = [
-    '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/volcengine-solutions-tourism.png~tplv-uwbnlip3yd-png.png',
-    '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/volcengine-solutions-finance.png~tplv-uwbnlip3yd-png.png',
-    '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/volcengine-solutions-medical.png~tplv-uwbnlip3yd-png.png',
-    '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/volcengine-solutions-automotive.png~tplv-uwbnlip3yd-png.png',
+    '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/a8c8cdb109cb051163646151a4a5083b.png~tplv-uwbnlip3yd-webp.webp',
+    '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/e278888093bef8910e829486fb45dd69.png~tplv-uwbnlip3yd-webp.webp',
+    '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/3ee5f13fb09879ecb5185e440cef6eb9.png~tplv-uwbnlip3yd-webp.webp',
+    '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/8361eeb82904210b4f55fab888fe8416.png~tplv-uwbnlip3yd-webp.webp',
   ];
 
   const imageSize = { width: 560, height: 200 };
+  const [visible, setVisible] = React.useState(false);
 
   return (
-    <Space direction="vertical" size={30} style={wrapperStyle}>
-      {srcList.map((src, key) => (
-        <Image
-          key={key}
-          {...imageSize}
-          src={src}
-          alt="lamp"
-          lazyload={{ threshold: 0.8 }}
-          // loader={<Skeleton image={{ style: imageSize }} text={false} animation />}
-        />
-      ))}
-    </Space>
+    <div>
+      <Space direction="vertical" size={30} style={wrapperStyle}>
+        {srcList.map((src, key) => (
+          <Image
+            key={key}
+            {...imageSize}
+            src={src}
+            alt="lamp"
+            // lazyload={{ threshold: 0.8 }}
+            // loader={<Skeleton image={{ style: imageSize }} text={false} animation />}
+          />
+        ))}
+      </Space>
+      <div>
+        <Button type="primary" onClick={() => setVisible(true)}>
+          Click me to preview multiple image
+        </Button>
+        <Image.PreviewGroup srcList={srcList} visible={visible} onVisibleChange={setVisible} />
+      </div>
+    </div>
   );
 }
 
