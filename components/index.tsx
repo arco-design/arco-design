6578ea29fb1880eb91d52678a24b636d050a268e
wangchen <wangchen.flsion@bytedance.com>
2024-02-28 17:46:42 +0800

    Release version 2.60.1

--- components/index.tsx
+++ components/index.tsx
@@ -229,4 +229,4 @@ export { default as Watermark } from './Watermark';
 export type { ImageProps, ImagePreviewProps, ImagePreviewActionProps, ImagePreviewGroupProps } from './Image/interface';
 export { default as Image } from './Image';
 
-export const version = '2.60.0';
+export const version = '2.60.1';
