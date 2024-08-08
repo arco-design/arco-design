e420fff4a45a3d8688bbaece9e7e0b1964c8b1b4
yinkaihui <yinkaihui@bytedance.com>
2024-08-08 17:06:22 +0800

    fix: affix

--- components/Tree/node.tsx
+++ components/Tree/node.tsx
@@ -258,7 +258,7 @@ function TreeNode(props: PropsWithChildren<NodeProps>, ref) {
             e.stopPropagation();
             // 当前节点正在被拖拽
             setState({ ...state, isDragging: true });
-            
+
             try {
               // ie throw error
               // firefox-need-it
@@ -266,7 +266,7 @@ function TreeNode(props: PropsWithChildren<NodeProps>, ref) {
             } catch (error) {
               // empty
             }
-            
+
             treeContext.onNodeDragStart && treeContext.onNodeDragStart(e, props);
           }}
           onDragEnd={(e) => {
